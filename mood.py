ab9b5fb13306c9ec9ecdb604f1143e3a8e7a3b90
yesuiii <e.yesui@letumongolia.com>
2024-10-30 21:58:00 +0800

    mood.py

--- mood.py
+++ mood.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from datetime import date
 import json
 import os
